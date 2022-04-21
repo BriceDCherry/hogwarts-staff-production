2efbe898691fcf75b6f1f9a552a52626630bc9da
bcherry4 <bcherry4@ford.com>
2022-04-21 16:07:11 -0600

    Add js css html changes

--- index.js
+++ index.js
@@ -29,5 +29,29 @@ const hogwartsStaffApiUrl = `http://hp-api.herokuapp.com/api/characters/staff`
 fetch(hogwartsStaffApiUrl)
     .then(response => response.json())
     .then(allHogwartsStaff => {
-        const staffContainer = document.querySelector("")
+        const staffContainer = document.querySelector(".staff-container");
+        staffContainer.innerHTML = ``;
+        console.log(allHogwartsStaff)
+
+        allHogwartsStaff.filter(staffInfo => staffInfo.image).forEach(staffInfo => {
+            const staffEl = document.createElement("li");
+            staffEl.classList.add("staff");
+
+            const staffName = document.createElement("h2")
+            staffName.classList.add("staff-name");
+            staffName.textContent = staffInfo.name;
+
+            const staffActor = document.createElement("p");
+            staffActor.classList.add("staff-actor");
+            staffActor.textContent = staffInfo.actor;
+
+            const staffImage = document.createElement("img");
+            staffImage.classList.add("staff-image");
+            staffImage.src = staffInfo.image;
+
+            staffEl.append(staffName);
+            staffEl.append(staffActor);
+            staffEl.append(staffImage);
+            staffContainer.append(staffEl);
+        })
     })
