17c886c6dd876977197709c27d9bd86986278ac0
bcherry4 <bcherry4@ford.com>
2022-04-22 11:31:24 -0600

    Add nav and links

--- index.js
+++ index.js
@@ -1,59 +1,59 @@
-const staff = {
-    "name": "Minerva McGonagall",
+const students = {
+    "name": "Harry Potter",
     "alternate_names": [],
     "species": "human",
-    "gender": "female",
+    "gender": "male",
     "house": "Gryffindor",
-    "dateOfBirth": "04-10-1925",
-    "yearOfBirth": 1925,
+    "dateOfBirth": "31-07-1980",
+    "yearOfBirth": 1980,
     "wizard": true,
-    "ancestry": "",
-    "eyeColour": "",
+    "ancestry": "half-blood",
+    "eyeColour": "green",
     "hairColour": "black",
     "wand": {
-        "wood": "",
-        "core": "",
-        "length": ""
+        "wood": "holly",
+        "core": "phoenix feather",
+        "length": 11
     },
-    "patronus": "tabby cat",
-    "hogwartsStudent": false,
-    "hogwartsStaff": true,
-    "actor": "Dame Maggie Smith",
+    "patronus": "stag",
+    "hogwartsStudent": true,
+    "hogwartsStaff": false,
+    "actor": "Daniel Radcliffe",
     "alternate_actors": [],
     "alive": true,
-    "image": "http://hp-api.herokuapp.com/images/mcgonagall.jpg"
+    "image": "http://hp-api.herokuapp.com/images/harry.jpg"
 }
 
 
 
-const hogwartsStaffApiUrl = `http://hp-api.herokuapp.com/api/characters/staff`
+const hogwartsStudentsApiUrl = `http://hp-api.herokuapp.com/api/characters/students`
 
-fetch(hogwartsStaffApiUrl)
+fetch(hogwartsStudentsApiUrl)
     .then(response => response.json())
-    .then(allHogwartsStaff => {
-        const staffContainer = document.querySelector(".staff-container");
-        staffContainer.innerHTML = ``;
-        console.log(allHogwartsStaff)
+    .then(allHogwartsStudents => {
+        const studentsContainer = document.querySelector(".students-container");
+        studentsContainer.innerHTML = ``;
+        console.log(allHogwartsStudents)
 
-        allHogwartsStaff.filter(staffInfo => staffInfo.image).forEach(staffInfo => {
-            const staffEl = document.createElement("li");
-            staffEl.classList.add("staff");
+        allHogwartsStudents.filter(studentsInfo => studentsInfo.image).forEach(studentsInfo => {
+            const studentsEl = document.createElement("li");
+            studentsEl.classList.add("students");
 
-            const staffName = document.createElement("h2")
-            staffName.classList.add("staff-name");
-            staffName.textContent = staffInfo.name;
+            const studentsName = document.createElement("h2")
+            studentsName.classList.add("students-name");
+            studentsName.textContent = studentsInfo.name;
 
-            const staffActor = document.createElement("p");
-            staffActor.classList.add("staff-actor");
-            staffActor.textContent = staffInfo.actor;
+            const studentsActor = document.createElement("p");
+            studentsActor.classList.add("students-actor");
+            studentsActor.textContent = studentsInfo.actor;
 
-            const staffImage = document.createElement("img");
-            staffImage.classList.add("staff-image");
-            staffImage.src = staffInfo.image;
+            const studentsImage = document.createElement("img");
+            studentsImage.classList.add("students-image");
+            studentsImage.src = studentsInfo.image;
 
-            staffEl.append(staffName);
-            staffEl.append(staffActor);
-            staffEl.append(staffImage);
-            staffContainer.append(staffEl);
+            studentsEl.append(studentsName);
+            studentsEl.append(studentsActor);
+            studentsEl.append(studentsImage);
+            studentsContainer.append(studentsEl);
         })
     })
